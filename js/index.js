f4976748a72fdbd19591413a08a0a8f11951d572
Ishaq Ali <ishaqniloy1996@gmail.com>
2022-04-10 05:00:08 +0600

    Completed the OTP popup

--- js/index.js
+++ js/index.js
@@ -391,15 +391,28 @@ function arrowRotation(btnNo) {
 
 //Close log in 
 
-function closeLogIn() {
-    var overlayElement = document.getElementsByClassName('log-in-section')[0];
-    var videoElement = document.getElementById('video');
-    overlayElement.style.display = 'none';
-    videoElement.pause();
+function closePopup() {
+    document.getElementsByClassName("sign-in-popup")[0].style.display = "none";
+    document.getElementsByClassName("create-account-popup")[0].style.display = "none";
+    document.getElementsByClassName("otp-popup")[0].style.display = "none";
 }
 
 
+// Opens Create Account Pop up
 
+function openCreateAccountPopup() {
+    document.getElementsByClassName("sign-in-popup")[0].style.display = "none";
+    document.getElementsByClassName("create-account-popup")[0].style.display = "flex";
+}
+
+//end
+
+//Opens OTP popup
+
+function showOtpPopup() {
+    document.getElementsByClassName("create-account-popup")[0].style.display = "none";
+    document.getElementsByClassName("otp-popup")[0].style.display = "flex";
+}
 
 
 
